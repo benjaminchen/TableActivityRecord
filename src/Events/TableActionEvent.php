f7b28a782a86097582a1eefab924fbd853011297
benjaminchen <benjaminchen.c@gmail.com>
2019-01-02 15:06:52 +0800

    update for safe getting value

--- src/Events/TableActionEvent.php
+++ src/Events/TableActionEvent.php
@@ -24,17 +24,19 @@ class TableActionEvent
     {
         // note: all value of data and tags should be string type
         $this->action = $action;
+        $modelArr = $model->toArray();
 
+        if (array_key_exists('admin_user_id', $modelArr)) $tags['admin_user_id'] = strval($modelArr['admin_user_id']);
         if ($adminUserId) $tags['admin_user_id'] = strval($adminUserId);
 
         foreach ($model->tags as $key) {
-            if ($model->{$key}) $tags[$key] = strval($model->{$key});
+            if (array_key_exists($key, $modelArr)) $tags[$key] = strval($model->{$key});
         }
 
         $this->table = $model->getTable();
         $this->tags = $tags;
         $this->tags["action"] = $action;
-        $this->data = array_diff_key($model->toArray(), $tags);
+        $this->data = array_diff_key($modelArr, $tags);
         $this->data = array_map('strval', $this->data);
         $this->time = exec('date +%s%N');
     }
